0c36319af0b4aa0249f747dc5a380dd50086c565
Vaishnav Ghenge <vaishnav.ghenge@noovosoft.com>
2024-09-27 15:10:50 +0530

    add: entrypoint on docker

---
+++ api/serializers.py
@@ -0,0 +1,39 @@
+# serializers.py
+from rest_framework import serializers
+from .models import Asset, EmployeeAsset, ChangeRequest, UpdateAsset, AddAsset, ReplaceAsset
+
+
+class AssetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Asset
+        fields = ['id', 'type', 'meta_data', 'serial_no']
+
+
+class EmployeeAssetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmployeeAsset
+        fields = ['id', 'asset', 'employee', 'from_date', 'to_date']
+
+
+class ChangeRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChangeRequest
+        fields = ['id', 'user', 'asset', 'type', 'meta_data', 'status']
+
+
+class UpdateAssetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UpdateAsset
+        fields = ['id', 'change_request', 'asset', 'meta_data']
+
+
+class AddAssetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AddAsset
+        fields = ['id', 'asset', 'change_request', 'status']
+
+
+class ReplaceAssetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReplaceAsset
+        fields = ['id', 'from_asset', 'to_asset', 'meta_data', 'change_request', 'from_date', 'to_date']
